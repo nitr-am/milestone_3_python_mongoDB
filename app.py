198d1d8a9babd511db8272b64c525cc8f572d6fd
nitr-am <martin7gomez@hotmail.com>
2019-11-13 17:37:34 +0000

    Adding add_book funcitonality

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'my_book_reading_history'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-ftnmb.mongodb.net/my_book_reading_history?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
+@app.route('/show_books')
 def hello():
-    return 'Hello World ...again'
+    return render_template("books.html", books=mongo.db.books.find())
+
+@app.route('/add_book')
+def add_book():
+    return render_template('addbook.html',
+    scores=mongo.db.scores.find(),
+    genres=mongo.db.genres.find())
+
+@app.route('/add_new_book', methods=['POST'])
+def add_new_book():
+    books = mongo.db.books
+    books.insert_one(request.form.to_dict())
+    return redirect(url_for('show_books'))    
 
 
 if __name__ == '__main__':
